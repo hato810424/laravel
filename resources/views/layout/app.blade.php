712b7d50bf7c31118ba42020de0b06099ec49b50
hato810424 <yukkuri.hatopoppo@gmail.com>
2022-12-31 08:58:09 +0900

    Laravel 完全に理解した

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- -->
+        <title>@yield('title') - {{ config('app.name') }}</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    </head>
+    <body id="body">
+        @include('layout.header')
+        @yield('content')
+        @include('layout.footer')
+    </body>
+</html>
---
+++ resources/views/temperate.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('title', 'てんぷれーと')
+
+@section('content')
+<h1>こんにちは！</h1>
+@endsection
